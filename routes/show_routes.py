e3df9b304a8f6cedeccb4701ab1b20ba23037ffb
danwood1992 <gunnerwoody449@gmail.com>
2024-01-15 08:57:06 +0000

    Fix shows template value

--- routes/show_routes.py
+++ routes/show_routes.py
@@ -6,38 +6,40 @@ from forms import ShowForm
 @app.route('/shows')
 def shows():
   
-  data=Show.query.all()
-  return render_template('pages/shows.html', shows=data)
+    shows=Show.query.all()
+    for show in shows:
+      print(show.artist.name)
+    return render_template('pages/shows.html', shows=shows)
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
-  form = ShowForm()
-  return render_template('forms/new_show.html', form=form)
+    # renders form. do not touch.
+    form = ShowForm()
+    return render_template('forms/new_show.html', form=form)
 
 @app.route('/shows/create', methods=['GET'])
 def create_show_form():
-  form = ShowForm()
-  return render_template('forms/new_show.html', form=form)
+    form = ShowForm()
+    return render_template('forms/new_show.html', form=form)
 
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
 
-  form = ShowForm(request.form)
-  show = Show(
-    artist_id = form.artist_id.data,
-    venue_id = form.venue_id.data,
-    start_time = form.start_time.data,
-  )
+    form = ShowForm(request.form)
+    show = Show(
+      artist_id = form.artist_id.data,
+      venue_id = form.venue_id.data,
+      start_time = form.start_time.data,
+    )
 
-  success = show.add()
-  if success:
-    flash('Show was successfully listed!')
-  else:
-    flash('An error occurred. Show could not be listed.')
+    success = show.add()
+    if success:
+      flash('Show was successfully listed!')
+    else:
+      flash('An error occurred. Show could not be listed.')
 
-  return render_template('pages/home.html')
+    return render_template('pages/home.html')
 
 
 def active_routes(active):
